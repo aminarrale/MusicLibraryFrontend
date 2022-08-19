dd71792358f8b2dabb578b48faae48b60d0df13c
Karim <aarrale12@gmail.com>
2022-08-18 20:53:41 -0500

    Axios requests made in App component + useEffect

--- musiclibraryfrontend/src/App.js
+++ musiclibraryfrontend/src/App.js
@@ -1,6 +1,38 @@
 
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+
+
+
+
 
 function App() {
+  const[songs, setSongs] = useState([]);
+
+
+  const fetchSongs = async () => {
+    try{
+      let response = await axios.get("http://127.0.0.1:8000/api/musiclibrary/")
+      setSongs(response.data)
+  
+    } catch(error) {
+      console.log(error.message)
+    }
+    console.log(songs)
+  }
+  
+  useEffect(() => {
+    fetchSongs()
+  }, []);
+
+
+
+
+
+
+
+  
   return (
     <div className="App">
       Hello world
