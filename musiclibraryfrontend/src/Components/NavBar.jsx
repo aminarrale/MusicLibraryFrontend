8c2344801eb8e16ca211539fe8cbef0333d52947
Karim <aarrale12@gmail.com>
2022-08-19 13:23:46 -0500

    NavBar created

---
+++ musiclibraryfrontend/src/Components/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = (props) => {
+    return (
+        <header class="nav-header">
+            <div class="nav-div">
+                <p>Music</p><p class="library">Library</p>
+            </div>
+        </header>
+    );
+}
+export default NavBar;
