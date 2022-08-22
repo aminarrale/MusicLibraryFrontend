1f8269cb38bfa426ce8f2c63a090cb0758e9626c
Karim <aarrale12@gmail.com>
2022-08-22 14:53:48 -0500

    Song component finished

--- musiclibraryfrontend/src/Components/Song.jsx
+++ musiclibraryfrontend/src/Components/Song.jsx
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
+
 const Song = (props) => {
-    return ( <div>
-        <h1>{props.song.title}</h1>
-        <div>{props.song.artist}</div>
-        <div>{props.song.album}</div>
-        <div>{props.song.release_date}</div>
-        <div>{props.song.genre}</div>
-    </div> );
+
+    const [title, setTitle] = useState('');
+    const [artist, setArtist] = useState('');
+    const [album, setAlbum] = useState('');
+    const [date, setDate] = useState('');
+    const [genre, setGenre] = useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+        let newSong = {
+            title: title,
+            artist: artist,
+            album: album,
+            release_date: date,
+            genre: genre
+        }
+        props.addNewSong(newSong);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type='text' value={title} placeholder='Title' className='form-field' onChange={(event) => setTitle(event.target.value)}/>
+            </div>
+            <div>
+                <input type='text' value={artist} placeholder='Artist' className='form-field' onChange={(event) => setArtist(event.target.value)}/>
+            </div>
+            <div>
+                <input type='text' value={album} placeholder='Album' className='form-field' onChange={(event) => setAlbum(event.target.value)}/>
+            </div>
+            <div>
+                <input type='genre' value={genre} placeholder='Genre' className='form-field' onChange={(event) => setGenre(event.target.value)}/>
+            </div>
+            <div>
+                <input type='date' value={date} className='form-field' onChange={(event) => setDate(event.target.value)}/>
+            </div>
+        </form>
+    )
 }
- 
 export default Song;
-
-
 
